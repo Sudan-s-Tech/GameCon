5db43b6592a83de4163266cc2b7a9efb1c202eb1
dawoodiddris <dawoodiddris@gmail.com>
2021-01-13 15:37:40 +0000

    edited website to reflect universal aigaming website

--- js/custom.js
+++ js/custom.js
@@ -39,7 +39,7 @@ $('a[href*="#"]')
 
 
   // Set the date we're counting down to
-var countDownDate = new Date("Jan 9, 2021 17:00:00").getTime();
+var countDownDate = new Date("Jan 19, 2021 15:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
